3efc646b808b3e99751e7830f9fba78edbe4fd26
Haris Lee <haris.lee168@gmail.com>
2023-07-24 11:42:47 +0800

    set path=* in the app.js for no match page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Home} from './components/Home'
 import {About} from './components/About'
 import {Navbar} from './components/Navbar'
 import {OrderSummary} from './components/OrderSummary'
+import {NoMatch} from './components/NoMatch'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<Home />}></Route>
           <Route path='about' element={<About/ >}></Route>
           <Route path='order-summary' element={<OrderSummary />}></Route>
+          <Route path='*' element={<NoMatch />}></Route>
         </Routes>
       </div>
     </div>
