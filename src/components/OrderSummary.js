7107eb7f23baf54e6ec0dda386d64982a78a3545
Haris Lee <haris.lee168@gmail.com>
2023-07-24 11:37:44 +0800

    link, navlink, and useNavigate

---
+++ src/components/OrderSummary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+
+export const OrderSummary = () => {
+  const navigate = useNavigate()
+  return (
+    <React.Fragment>
+      <div>Order Confirmed</div>
+      <button onClick={() => {navigate(-1)}}>Back button</button>
+    </React.Fragment>
+  )
+}
